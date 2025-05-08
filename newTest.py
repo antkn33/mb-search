53ba561ec2ac1fa326d3b58577580ef0b33fd67a
Anthony Scott <antkn33@gmail.com>
2025-05-08 14:17:30 -0500

    partially working artist info newTest file

--- newTest.py
+++ newTest.py
@@ -31,10 +31,20 @@ musicbrainzngs.set_useragent("YourApplicationName", "0.1", "youremail@example.co
 
 # Assume you have a release ID
 release_id = "b7f998ec-32ca-43c7-a93e-d6d1456df45c"
-release_data = musicbrainzngs.get_release_by_id(release_id, includes="recordings")
+release_data = musicbrainzngs.get_release_by_id(release_id, includes=["recordings", "recording-level-rels", "work-level-rels",
+    "artist-rels", "work-rels"])
 # pprint.pp(release_data)
+
 # List tracks from the album+
 for medium in release_data['release']['medium-list']:
     # pprint.pp(medium)
     for track in medium['track-list']:
-        print(f"Track: {track['recording']['title']}")
+        print(f"Track {track['position']}: {track['recording']['title']}")
+        try:
+            for artist in track['recording']['artist-relation-list']:
+                if artist['type'] == 'instrument':
+                    print(f"\t{artist['artist']['name']}:")
+                    for intrustment in artist['attribute-list']:
+                        print(f" \t\t{intrustment}")
+        except:
+            print("info not available")
