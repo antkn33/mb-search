31743ff3ad3f97933e90c620854d07d91634b01d
Anthony Scott <antkn33@gmail.com>
2025-05-08 19:25:59 -0500

    clean up personel listing works

--- newTest.py
+++ newTest.py
@@ -5,46 +5,23 @@ import pprint
 
 # Set the user agent for your application
 musicbrainzngs.set_useragent("YourApplicationName", "0.1", "youremail@example.com")
- 
-
-# result = musicbrainzngs.search_artists(artist="Radiohead")
-
-# # Display information about the first artist in the search result
-# artist = result['artist-list'][0]
-# print(f"Artist: {artist['name']}, ID: {artist['id']}")
-
-# artist_id = artist['id']
-# artist_data = musicbrainzngs.get_artist_by_id(artist_id, includes=["releases"])
-# print(artist_data)
-
-# Print album titles
-# for release in artist_data['artist']['release-list']:
-#    # pprint.pp(release)
-
-#     print(f"Album: {release['title']}, ID: {release['id']}")
-
-
-import musicbrainzngs
-import pprint
-# Set the user agent for your application
-musicbrainzngs.set_useragent("YourApplicationName", "0.1", "youremail@example.com")
 
 # Assume you have a release ID
-release_id = "b7f998ec-32ca-43c7-a93e-d6d1456df45c"
+release_id = "b2784fc1-283f-4af7-8dc6-5061764e7076"
 release_data = musicbrainzngs.get_release_by_id(release_id, includes=["recordings", "recording-level-rels", "work-level-rels",
     "artist-rels", "work-rels"])
-# pprint.pp(release_data)
 
 # List tracks from the album+
 for medium in release_data['release']['medium-list']:
     # pprint.pp(medium)
     for track in medium['track-list']:
-        print(f"Track {track['position']}: {track['recording']['title']}")
+        print(f"Track {track['number']}: {track['recording']['title']}")
         try:
             for artist in track['recording']['artist-relation-list']:
                 if artist['type'] == 'instrument':
-                    print(f"\t{artist['artist']['name']}:")
                     for intrustment in artist['attribute-list']:
-                        print(f" \t\t{intrustment}")
+                        print(f"\t{artist['artist']['name']}: {intrustment}")                    
+                        # print(f" \t\t{intrustment}")
         except:
             print("info not available")
+
